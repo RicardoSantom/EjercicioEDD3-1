0f7f2a113ecbf2128ddc82d05f652fcd931bc53e
srunzu15jcyl <73907922+srunzu15jcyl@users.noreply.github.com>
2020-11-09 08:49:35 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/ejercicioedd3/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicioedd3;
+
+import java.util.Scanner;
+
+/**
+ * Calculates the area of a triangle.
+ *
+ * @author marcos
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+
+        System.out.println("¿Cuál es el altura del triángulo?");
+        Scanner scanner = new Scanner(System.in);
+        triangle.setHeight(scanner.nextFloat());
+
+        System.out.println("¿Cuál es el base del triángulo?");
+        scanner = new Scanner(System.in);
+        triangle.setBase(scanner.nextFloat());
+
+        float area = triangle.calculateArea();
+        System.out.println("El área del triángulo es " + area);
+
+        float fixedHeight = 5;
+        float fixedBase = 4;
+
+        Triangle fixedTriangle = new Triangle(fixedBase, fixedHeight);
+        System.out.println("El área del triángulo fijado es " + fixedTriangle.calculateArea());
+
+    }
+
+}
---
+++ src/main/java/com/mycompany/ejercicioedd3/Triangle.java
@@ -0,0 +1,48 @@
+package com.mycompany.ejercicioedd3;
+
+/**
+ * It contains the data of a triangle and allows us to calculate its area.
+ * @author Marcos Unzueta Puente
+ * @version 1.0
+ */
+public class Triangle {
+    private float base;
+    private float height;
+
+    public Triangle() {
+        
+    }
+
+    public Triangle(float base, float height) {
+        this.base = base;
+        this.height = height;
+    }
+    
+    
+/**
+ * This function returns the value of the area of the triangle.
+ * @return area of the triangle.
+ */
+    float calculateArea(){
+        return (base * height)/2;
+    }
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    
+    
+}
